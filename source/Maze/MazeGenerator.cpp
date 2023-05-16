672d3b2e463de6dafd9d001cbb3c3176e26b988d
Guus <guussaarloos@outlook.com>
2023-05-16 16:42:46 +0200

    Truly random spawnpoints are now a thing

--- source/Maze/MazeGenerator.cpp
+++ source/Maze/MazeGenerator.cpp
@@ -10,7 +10,7 @@
 
 #include <iostream>
 
-MazeGenerator::MazeGenerator() {
+MazeGenerator::MazeGenerator() : x(0), y(0) {
 	// added pointer of Texture to the vector.
 	mazeTextures.push_back(new Texture("resource/textures/Floor4.png"));
 	mazeTextures.push_back(new Texture("resource/textures/Bush_Texture4.png"));
@@ -30,18 +30,19 @@ void MazeGenerator::Generate(const int& sizeOfMazeX, const int& sizeOfMazeZ) {
 	const glm::vec2 leftBack = glm::vec2(0, sizeOfMazeZ);
 	const glm::vec2 rightBack = glm::vec2(sizeOfMazeX, sizeOfMazeZ);
 
-	spawnPoint = glm::vec3(-1, 0, -1);
+	spawnPoint = SetSpawnPoint(sizeOfMazeX, sizeOfMazeZ);
 
-	bool flag = false;
 	for (int z = 0; z <= sizeOfMazeZ; z++) {
 		std::vector<Tile*> file;
 		for (int x = 0; x <= sizeOfMazeX; x++) {
-			// check if it's an edge.
-			if (z == sizeOfMazeZ || z == 0 || x == 0 || x == sizeOfMazeX) {
-				file.push_back(PlaceWall(x, z));
+
+			// check if it's an edge. If so, place Wall.
+			if (IsEdge(x, z, sizeOfMazeX, sizeOfMazeZ)) {
+				file.push_back(PlaceWall((float)x, (float)z));
+			}
 			// not an edge
-			} else {
-				file.push_back(PlaceFloor(x, z));
+			else {
+				file.push_back(PlaceFloor((float)x, (float)z));
 			}
 		}
 		maze.push_back(file);
@@ -68,6 +69,41 @@ void MazeGenerator::DrawMaze() {
 			tile->draw();
 		}
 	}
+}
+
+glm::vec3 MazeGenerator::SetSpawnPoint(const int &sizeX, const int &sizeZ) {
+	const int edgesOfMaze = (sizeX - 4) * (sizeZ - 4);
+
+	// create a number between the edges of the maze and 4. (standard 36 if maze is 10x10).
+	int iterator = (rand() % edgesOfMaze) + 4;
+
+	std::cout << "iterator: " << iterator << "\n";
+	for (int x = 0; x <= sizeX; x++)
+		for (int z = 0; z <= sizeZ; z++) {
+			if (NextToEdge(x, z, sizeX, sizeZ))
+				iterator--;
+			if (iterator == 0) {
+				std::cout << "spawnpos: " << x << ',' << z << "\n";
+				return glm::vec3((float)x * -1, 0, (float)z * -1);
+			}
+		}
+}
+
+bool MazeGenerator::NextToEdge(const int& x, const int& z, const int& sizeX, const int& sizeZ) {
+	return (x == (sizeX - 1) && x != 0|| x == 1 || z == (sizeZ - 1) && z != 0 || z == 1);
+		
+}
+
+bool MazeGenerator::IsEdge(const int& x, const int& z, const int& sizeX, const int& sizeZ) {
+	return (x == sizeX || x == 0 || z == 0 || z == sizeZ);
+}
+
+bool MazeGenerator::IsCorner(const int& x, const int& z, 
+	const glm::vec2& lf, const glm::vec2& lb, 
+	const glm::vec2& rf, const glm::vec2& rb) {
+
+	glm::vec2 xy = glm::vec2(x, y);
+	return xy == lf || xy == lb || xy == rf || xy == rb;
 }
 
 Tile* MazeGenerator::PlaceWall(const float &x, const float &z)
--- source/headers/Maze/MazeGenerator.h
+++ source/headers/Maze/MazeGenerator.h
@@ -22,6 +22,12 @@ private:
 
 	std::vector<Texture*> mazeTextures;
 
+	bool IsCorner(const int& x, const int& z, const glm::vec2& lf, const glm::vec2& lb, const glm::vec2& rf, const glm::vec2& rb);
+	bool IsEdge(const int& x, const int& z, const int& sizeX, const int& sizeZ);
+	bool NextToEdge(const int& x, const int& z, const int& sizeX, const int& sizeZ);
+
+	glm::vec3 SetSpawnPoint(const int&, const int&);
+
 	Tile* PlaceWall(const float& x, const float& z);
-	Tile* PlaceBush(const float& x, const float& z);
+	Tile* PlaceFloor(const float& x, const float& z);
 };
